4939b9c0dad6f0a4a65e298378b4abafe984bea8
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-04-08 13:46:05 +0300

    feat: define local database module and for deploying

--- src/module/database.module.ts
+++ src/module/database.module.ts
@@ -20,3 +20,23 @@ import { User } from '../model/user.entity';
   ],
 })
 export class DatabaseModule {}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        username: configService.get('POSTGRES_USER'),
+        database: configService.get('POSTGRES_DATABASE'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        port: configService.get('POSTGRES_PORT'),
+        entities: [Dummy, User],
+        synchronize: true,
+        migrationsRun: true,
+      }),
+    }),
+  ],
+})
+export class LocalDatabaseModule {}
