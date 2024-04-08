2f2bf5da38ecf57ab08879d280f9bf60bff01327
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-04-08 13:46:05 +0300

    feat: define local database module and for deploying

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbHealthModule } from './module/dummy.module';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './module/database.module';
+import { DatabaseModule, LocalDatabaseModule } from './module/database.module';
 import { UserModule } from './module/user.module';
 import { AuthModule } from './auth/auth.module';
 
